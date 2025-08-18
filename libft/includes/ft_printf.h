ef339a22d0e11aacb8f955028e7aa760cf9335ad
Nikita Luchinin <nluchini@1-D-6.42heilbronn.de>
2025-08-18 15:32:22 +0200

    Add new ft_printf_fd

--- libft/includes/ft_printf.h
+++ libft/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 20:43:25 by nluchini          #+#    #+#             */
-/*   Updated: 2025/07/29 13:32:10 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/08/14 20:21:19 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,12 @@
  */
 int	ft_printf(const char *format, ...);
 
+/**
+ * @brief The replica of the printf function from the C standard library.
+ * This function formats and prints a string to the specified file descriptor.
+ * @param  fd: The file descriptor to which the output will be written.
+ * @param  format: The format string that specifies how to format the output.
+ * @retval The number of characters printed. -1 if an error occurs.
+ */
+int	ft_printf_fd(int fd, const char *format, ...);
 #endif
